f27f5ad71c3b6d39323619d2107f0ca2cdbff028
John Evans <john@jhevans.co.uk>
2019-10-27 22:19:47 +0000

    Empty main code file
    Initial code commit; builds and runs (does nothing though!)

---
+++ src/main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "freertos/queue.h"
+
+void app_main(void)
+{
+    //nop
+}
