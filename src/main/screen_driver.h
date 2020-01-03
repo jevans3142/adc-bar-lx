72a3fb11533493d059461970673398c60185501d
John Evans <djjevans+github@googlemail.com>
2020-01-03 11:35:30 +0000

    Remove draw_circle function prototype
    Should have been removed in last commit

--- src/main/screen_driver.h
+++ src/main/screen_driver.h
@@ -40,7 +40,6 @@ void clear_display(void);
 void draw_pixel(int16_t x, int16_t y, uint16_t type);
 void draw_line(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t type);
 void draw_rect(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint16_t outline, uint16_t fill);
-void draw_circle(int16_t xc, int16_t yc, int16_t r, uint16_t outline, uint16_t fill);
 void draw_string(uint_fast8_t x, uint_fast8_t y, const char* s, uint_fast8_t scaling, uint_fast8_t type);
 
 #endif  
